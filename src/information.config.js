2ddff126f015e0bdb0d501a5a0524dfb44e9c1b5
Arman Abkar <armanw@outlook.com>
2021-05-07 00:32:32 +0430

    initial commit

---
+++ src/information.config.js
@@ -0,0 +1,32 @@
+export const Information = {
+  name: "Arman Abkar",
+  profilePicture: "",
+  position: "Software Developer",
+  location: "Isfahan, Iran",
+  about: [
+    "Professional & experienced <strong>Software Developer</strong> with a passion for continuous learning and teaching cutting-edge technologies. Specialized in Client-Side (Frontend) technologies, frameworks and methodologies in order to build performant, cross-platform and scalable applications with high level of functionality and interactive features that will affect directly on business growth and meet customer expectations.",
+    "When he is not working on computer, you can find him lifting weights in gym, playing video games, watching movies/anime or reading tech related books.",
+  ],
+  skills:
+    "JavaScript, Swift, React, Vue, Angular, Node.js, Flutter, Dart, Docker.",
+  interests: "Blockchain, Machine Learning, Mobile App Development.",
+  experiences: [
+    // {
+    //   position: "Software Engineer",
+    //   company: "Apple",
+    //   location: "Cupertino, California",
+    //   date: "Sep 2019 - Present",
+    // },
+  ],
+  email: "armanabkar@gmail.com",
+  socialMedia: {
+    LinkedIn: "https://www.linkedin.com/in/armanabkar/",
+    Github: "https://github.com/armanabkar",
+    StackOverflow: "",
+    Telegram: "https://t.me/BlackCraftCult",
+    Twitter: "https://twitter.com/armanabkar",
+    Instagram: "https://www.instagram.com/armanabkar/",
+  },
+  keywords:
+    "Arman Abkar, armanabkar, Software Engineer, Frontend Web Developer, iOS Developer",
+};
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Information } from "../information.config";
+</script>
+
+<svelte:head>
+  <title>{Information.name}</title>
+</svelte:head>
+
+<div class="container">
+  {#each Information.about as info}
+    <p>
+      {@html info}
+    </p>
+  {/each}
+</div>
+
+<style>
+</style>
